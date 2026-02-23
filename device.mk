a240d00d84b2173cd4bbf054f603257a86d5c4b8
Rancemxn <209220634+Rancemxn@users.noreply.github.com>
2026-02-23 16:33:10 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -29,6 +29,6 @@ PRODUCT_PACKAGES += \
     update_engine_sideload
 
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
-PRODUCT_PACKAGES +=
-android.hardware.fastboot@1.0-impl-mock
-fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
